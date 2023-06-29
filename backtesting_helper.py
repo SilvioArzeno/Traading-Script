36b5e9a7cb6a87e739f85c1f1b6f17bc9d7961b7
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2023-06-29 12:25:47 -0400

    Fixes Max Loss and Max Profit Logic

--- backtesting_helper.py
+++ backtesting_helper.py
@@ -3,45 +3,64 @@ days = []
 total = 0
 current_sum = 0
 win_rate = 0
+max_loss_index = float('inf')
+max_profit_index = float('inf')
 
 # Loop through days and get input
 print("Enter daily amounts for the month. Press ENTER to stop.")
+day_index = 0
 while True:
     # Get input from user
-    amount_str = input(f"Day {len(days)+1}: ")
-    
+    amount_str = input(f"Day {day_index + 1}: ")
+
     # Check for empty input
     if amount_str == "":
         break
-    
+
     try:
         amount = int(amount_str)
-        
+
         # Add amount to list and total
         days.append(amount)
         total += amount
-        
+
         # Update win rate
         if amount > 0:
             win_rate += 1
-            
+
+
     except ValueError:
         print("Invalid input. Please enter a number.")
         continue
+
+    # Update current sum
+    current_sum += amount
+    day_index += 1
 
 # Calculate maximum sum of consecutive days
 current_sum = 0
 max_sum = 0
 min_sum = 0
-for amount in days:
+for index,amount in enumerate(days):
     current_sum = current_sum + amount
+   
+    # Check if max loss or max profit is hit
+    if current_sum <= min_sum:
+        max_loss_index = index
+    if current_sum >= max_sum :
+        max_profit_index = index
+    
     max_sum = max(max_sum, current_sum)
     min_sum = min(min_sum, current_sum)
+
+# Determine which was hit first
+hit_first = "Max Loss" if max_loss_index < max_profit_index else "Max Profit"
 
 # Print results
 print(f"Monthly total: {total}%")
 print(f"Max Profit: {max_sum}%")
 print(f"Max Loss: {min_sum}%")
-print(f"Win rate: {(win_rate/len(days))*100}%")
+print(f"Win rate: {(win_rate / len(days)) * 100}%")
+print(f"Hit first: {hit_first}")
 
 input('PRESS ENTER TO EXIT')
